2ae2c05eb5521e79d0dc52e370d36618c1a475e6
rdipardo <59004801+rdipardo@users.noreply.github.com>
2024-08-21 17:19:49 -0400

    Add a package to referenced library project

--- src/Hello.World/Greeter.cs
+++ src/Hello.World/Greeter.cs
@@ -1,6 +1,13 @@
 ﻿namespace Hello.World;
 
+using Figgle;
+
 public class Greeter
 {
     public static string Greet() => "Hello, World!";
+
+    private string FiggleMeThis(string message)
+    {
+        return FiggleFonts.Standard.Render(message);
+    }
 }
